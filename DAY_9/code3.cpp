e165c7f7af1d1a104af29a0590b93d18f067667a
KARTIKCU <chauhan5445kartik@gmail.com>
2026-07-06 19:32:12 +0530

    DAY_9 Completed

---
+++ DAY_9/code3.cpp
@@ -0,0 +1,13 @@
+//Write a program to Print repeated character pattern
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) cout << (char)('A' + i);
+        cout << endl;
+    }
+    return 0;
+}
