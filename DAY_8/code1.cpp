fbccf03e6d76619672ede7dc80622c00ab74ce66
KARTIKCU <chauhan5445kartik@gmail.com>
2026-07-06 19:31:24 +0530

    DAY_8 Completed

---
+++ DAY_8/code1.cpp
@@ -0,0 +1,13 @@
+// WAP  to Print half pyramid patterns
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
